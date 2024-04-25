a862258487b73d9c6ecaf8b614181ebc18bb2a76
nicolas2038 <168097290+nicolas2038@users.noreply.github.com>
2024-04-25 17:42:00 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,15 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 
 osm.addTo(map);
+fetch('tartu_city_districts_edu.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: function(feature, layer) {
+        if (feature.properties && feature.properties.NIMI) {
+          layer.bindPopup(feature.properties.NIMI);
+        }
+      }
+    }).addTo(map);
+  });
 
