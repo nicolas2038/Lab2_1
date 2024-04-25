85220a9f03a9a2d4c758c9666886c205c30f21a3
nicolas2038 <168097290+nicolas2038@users.noreply.github.com>
2024-04-25 17:39:27 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
-let map = L.map('map').setView([58.373523, 26.716045], 12)
-const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
- maxZoom: 19,
- attribution: 'OpenStreetMap contributors',
-})
-osm.addTo(map)
+var map = L.map('map').setView([58.373523, 26.716045], 12);
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'OpenStreetMap contributors'
+});
+
+osm.addTo(map);
+
