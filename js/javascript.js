c3103f6500f7efd2bec04df247341a5d56b23ffe
nicolas2038 <168097290+nicolas2038@users.noreply.github.com>
2024-05-02 20:40:03 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,16 @@
-var map = L.map('map').setView([58.373523, 26.716045], 12);
-var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'OpenStreetMap contributors'
-});
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: 'OpenStreetMap contributors',
+})
+osm.addTo(map)
 
-osm.addTo(map);
-fetch('tartu_city_districts_edu.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: function(feature, layer) {
-        if (feature.properties && feature.properties.NIMI) {
-          layer.bindPopup(feature.properties.NIMI);
-        }
-      }
-    }).addTo(map);
-  });
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
